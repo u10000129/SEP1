797e93c52d892600397ecde069e8330e09825736
Kuan0911 <lihaokuan911@gmail.com>
2017-03-22 22:24:26 +0800

    hi

---
+++ src/gradeSystem/Grades.java
@@ -0,0 +1,22 @@
+
+public class Grades {
+	String name;
+	String ID;
+	int lab1, lab2, lab3, midTerm, finalExam, totalGrade;
+	
+	Grades(String name, String ID, int lab1, int lab2, int lab3, int midTerm, int finalExam) {
+		this.name = name;
+		this.ID = ID;
+		this.lab1 = lab1;
+		this.lab2 = lab2;
+		this.lab3 = lab3;
+		this.midTerm = midTerm;
+		this.finalExam = finalExam;
+	}
+	
+	public int calculateTotalGrade() {
+		totalGrade = lab1+lab2+lab3+midTerm+finalExam;
+		return totalGrade;		
+	}
+
+}
---
+++ src/gradeSystem/UI.java
@@ -0,0 +1,27 @@
+
+public class UI {
+	
+	UI() {
+		
+	}
+	
+	public void checkID() {
+		return null;
+	}
+	
+	public void promptCommand() {
+		
+	}
+	
+	public void promptID() {
+		
+	}
+	
+	public void showFinishMsg() {
+		
+	}
+	
+	public void showWelcomeMsg() {
+		
+	}
+}
