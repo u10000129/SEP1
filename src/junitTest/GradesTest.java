8e6e730dfc511aed710fece1a7ebd1fde65f4265
u10000129 <u10000129@gmail.com>
2017-03-23 15:36:34 +0800

    add junitTest and input testcase

---
+++ src/junitTest/GradesTest.java
@@ -0,0 +1,27 @@
+package junitTest;
+import gradeSystem.Grades;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GradesTest {
+	Grades grade;
+
+	@Before
+	public void setUp() throws Exception {
+		grade = new Grades("sdd","123",1,1,1,1,1);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		grade = null;
+	}
+
+	@Test
+	public void test() {
+		assertEquals(5, grade.calculateTotalGrade());
+	}
+
+}
