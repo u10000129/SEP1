bf00925f72f69eafe001804dd3aa86f989db76ca
u10000129 <u10000129@gmail.com>
2017-03-24 12:01:52 +0800

    modify show average message

--- src/gradeSystem/UI.java
+++ src/gradeSystem/UI.java
@@ -37,7 +37,7 @@ public class UI {
 	public void promptCommand() {
 		System.out.println("Enter Command: 1).G - Show your grade");
 		System.out.println("               2).R - Show your rank");
-		System.out.println("               3).A - Show your average grade");
+		System.out.println("               3).A - Show average grade of each exam");
 		System.out.println("               4).W - Modify weight on each exam");
 		System.out.println("               5).E - Exit Menn");
 	}
