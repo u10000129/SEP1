293a62ba845185c4b117ed146d52b874b7fe8c47
u10000129 <u10000129@gmail.com>
2017-03-26 23:11:37 +0800

    add headers to each method

--- src/gradeSystem/Main.java
+++ src/gradeSystem/Main.java
@@ -7,6 +7,10 @@ public class Main {
 	static UI ui = new UI();
 	static Scanner scanner = new Scanner(System.in);
 	
+	/**IdExist Usage
+	 * @return whether student ID that user input exist in system.
+	 */
+	
 	private static boolean IdExist() {
 		if(ui.checkID(input)==false) {
 			ui.showStudentIdWrongMsg();
@@ -14,7 +18,8 @@ public class Main {
 		}
 		return true;
 	}
-	/*
+	/** UserInput Usage
+	 * @return 0, 1 or 2 to distinguish user input. 
 	 * 0, shut down system
 	 * 1, user id wrong, re-ask user to enter ID or Quit
 	 * 2, continue to Function Mode
@@ -32,17 +37,26 @@ public class Main {
 		return 2;
 	}
 	
+	/** functionMode Usage
+	 * 1st, Welcome user log in system, and indicate them their are 5 different commands(G, R, A, W, E).
+	 * 2nd, ask user enter command or log out.
+	 */
 	private static void functionMode() {
 		// show some welcome message
 		ui.showWelcomeMsg();
 		ui.promptCommand();
 		// ask user to enter G, R, A, W or E
 		while(true) {
+			System.out.print("Please enter your command: ");
 			input = scanner.nextLine();
 			if(ui.checkCommand(input)==false) break;
 		}
 	}
 	
+	/**main Usage
+	 * 1st, welcome user and ask them to use student ID to log in.
+	 * 2nd, ask user to enter command.
+	 */
 	public static void main(String[] args) {
 		while(true) {
 			// indicate user to enter student ID or quit System
--- src/gradeSystem/UI.java
+++ src/gradeSystem/UI.java
@@ -3,10 +3,17 @@ public class UI {
 	GradeSystems gradeSystems;
 	String curStudentID;
 	
+	/**Constructor Usage
+	 * construct a grade system.
+	 */
 	UI() {
 		gradeSystems = new GradeSystems();
 	}
 	
+	/** CheckCommand Usage
+	 * @param cmd - the command user input (G, R, A, W or E).
+	 * @return false - if user want to log out. 
+	 */
 	public boolean checkCommand(String cmd) {
 		if(cmd.toUpperCase().equals("G")) gradeSystems.showGrade(curStudentID);
 		else if(cmd.toUpperCase().equals("R")) gradeSystems.showRank(curStudentID);
@@ -17,31 +24,50 @@ public class UI {
 		return true;
 	}
 	
+	/** checkID usage
+	 * @param studentID - the student ID user used to log in.
+	 * @return whether student ID exist in system.
+	 */
 	public boolean checkID(String studentID) {
 		curStudentID = studentID;
 		return gradeSystems.containID(studentID);
 	}
 	
+	/** promptCommand
+	 * Indicate user there are 5 command available (G, R, A, W or E)
+	 */
 	public void promptCommand() {
-		System.out.println("Enter Command: 1).G - Show your grade");
-		System.out.println("               2).R - Show your rank");
-		System.out.println("               3).A - Show average grade of each exam");
-		System.out.println("               4).W - Modify weight on each exam");
-		System.out.println("               5).E - Exit Menn");
+		System.out.println("Support Command: 1).G - Show your grade");
+		System.out.println("                 2).R - Show your rank");
+		System.out.println("                 3).A - Show average grade of each exam");
+		System.out.println("                 4).W - Modify weight on each exam");
+		System.out.println("                 5).E - Exit Menn");
 	}
 	
+	/** promptID usage
+	 * indicate user use ID to log in or quit system.
+	 */
 	public void promptID() {
-		System.out.print("Enter 'student ID' or 'Q' to quit System: ");
+		System.out.print("Enter 'student ID' to log in or 'Q' to quit System: ");
 	}
 	
+	/** showFinishMsg Usage
+	 * print out finish message.
+	 */
 	public void showFinishMsg() {
 		System.out.println("System shut down.");
 	}
 	
+	/** showWelcomeMsg Usage
+	 * Welcome user who log in.
+	 */
 	public void showWelcomeMsg() {
 		System.out.println("Welcome "+gradeSystems.getStudentName(curStudentID));
 	}
 	
+	/** showStudentIdWrondMsg
+	 * Tell user the input student ID is wrong.
+	 */
 	public void showStudentIdWrongMsg() {
 		System.out.println("Student ID dosen't exit in our System!");
 	}
