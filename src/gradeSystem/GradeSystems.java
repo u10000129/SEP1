7b0d88ae9d712211b0ceae260d9fe8861c587e86
Kuan0911 <lihaokuan911@gmail.com>
2017-03-27 22:18:14 +0800

    fix update weight add method header

--- src/gradeSystem/GradeSystems.java
+++ src/gradeSystem/GradeSystems.java
@@ -20,7 +20,21 @@ public class GradeSystems {
 								"final exam    ",
 								"total grade   "};
 	
-	
+	/**Constructor Usage
+	 * read input data and keep them in a linked list.
+	 * 
+	 * @throws IOException - throws exception when error occur during input.
+	 * @throws UnsupportedEncodingException - throws exception when input encoding is not supported.
+	 * @throws FileNotFoundException - throws exception when input file is not found.
+	 * 
+	 * pseudo code: 
+	 * 1.create a LinkedList instance for aList to store aGrade.
+	 * 2.read input file line by line.
+	 * 3.split the input line by space and use it to initialize aGrade for the student.
+	 * 4.repeat step 2 and 3 until the end of the file.
+	 * 
+	 * Time estimate: O(N), N for number of students.
+	 */
 	public GradeSystems() {
 		aList = new LinkedList<Grades>();
 
@@ -47,11 +61,22 @@ public class GradeSystems {
 			System.out.println("Loading error");
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Loading error");
 			e.printStackTrace();
 		}
 	}
-	
+	/**containID usage
+	 * check if ID is in GradeSystem
+	 * @param ID - ID to check.
+	 * @return true if ID exists. otherwise, return false.
+	 * 
+	 * pseudo code: 
+	 * 1.go through aList and check if ID exists.
+	 * 2.if ID match, return true.
+	 * 3.if ID is not fount after going through the list, return false.
+	 * 
+	 * Time estimate: O(N), N for number of students. 
+	 */
 	public boolean containID(String ID) {
 		for (Grades temp : aList) {
             if(temp.ID.equals(ID)) return true;
@@ -59,6 +84,18 @@ public class GradeSystems {
 		return false;
 	}
 	
+	/**showGrade usage
+	 * show the student's grade on console.
+	 * 
+	 * @param ID - ID to search.
+	 * 
+	 * pseudo code: 
+	 * 1.go through aList and find the aGrade that matches the ID.
+	 * 2.print the studnet's grades on console.
+	 * 3.if the grade is lower than 60, add a '*' after the score.
+	 * 
+	 * Time estimate: O(N), N for number of students. 
+	 */
 	public void showGrade(String ID) {		
 		for (Grades temp : aList) {
             if(temp.ID.equals(ID)) {
@@ -72,7 +109,22 @@ public class GradeSystems {
             }
         }		
 	}
-
+	/**showRank usage
+	 * show the student's rank on console.
+	 * 
+	 * @param ID - ID to search.
+	 * @return student's rank. 0 if ID not found.
+	 * 
+	 * pseudo code: 
+	 * 1.reset rank to 0.
+	 * 2.go through aList to find aGrade of the student to search.
+	 * 3.set rank to 1.
+	 * 4.go through aList, if the total grade is higher than the student to search, add 1 to rank.
+	 * 5.after going through aList, return rank.
+	 * 6.if ID is not found, return 0.
+	 * 
+	 * Time estimate: O(N), N for number of students. 
+	 */
 	public int showRank(String ID) {
 		int rank = 0;
 		for (Grades current : aList) {
@@ -81,13 +133,25 @@ public class GradeSystems {
             	for (Grades temp : aList) {
                     if(temp.totalGrade > current.totalGrade) rank = rank+1; 
                 }
-            	System.out.println(current.name+"李威廷排名第"+rank);
+            	System.out.println(current.name+"排名第"+rank);
             	return rank;
             }           
 		}
 		return 0;		
 	}
-	
+	/**updateWeights usage
+	 * set the weights for grades.
+	 * 
+	 * pseudo code: 
+	 * 1.show old weights on console
+	 * 2.let user input the new weights.
+	 * 3.show new weights on console.
+	 * 4.ask if the new weights is correct.
+	 * 5.update the weights by new weights if the user inputs 'y' or 'Y'.
+	 * 6.recalculate all students' total grades.
+	 * 
+	 * Time estimate: O(N), N for number of students. 
+	 */
 	public void updateWeights() {
 		System.out.println("舊配分");
 		for(int i=0;i<5;i++) System.out.println(gradeItemList[i]+"         "+Math.round(weights[i]*100)+"%");		
@@ -100,14 +164,28 @@ public class GradeSystems {
 			weights_temp[i] = scanner.nextFloat()/100;
 		}
 		System.out.println("請確認新配分");
-		for(int i=0;i<5;i++) System.out.println(gradeItemList[i]+"         "+Math.round(weights[i]*100)+"%");		
+		for(int i=0;i<5;i++) System.out.println(gradeItemList[i]+"         "+Math.round(weights_temp[i]*100)+"%");		
 		System.out.print("以上正確嗎? Y (Yes) 或 N (No):");		
-		if(scanner.next().equals("Y")) weights = weights_temp.clone();
+		if(scanner.next().toUpperCase().equals("Y")) {
+			weights = weights_temp.clone();
+			System.out.println("更新成功");	
+		}
+		else System.out.println("取消更新配分");		
 		//scanner.close();
 		for (Grades temp : aList) temp.calculateTotalGrade(weights);
 		
 	}
-	
+	/**showAverages usage
+	 * show averages of every items in grades on console.
+	 * 
+	 * pseudo code: 
+	 * 1.new a instance of averageList to store every averages.
+	 * 2.go through aList to get the sum of each item.
+	 * 3.divided each sum by the number of student.
+	 * 4.show averages score on console.
+	 * 
+	 * Time estimate: O(N), N for number of students. 
+	 */
 	public void showAverages() {
 		int[] averageList = new int[6];		
 		for (Grades temp : aList) {
@@ -120,7 +198,17 @@ public class GradeSystems {
 			System.out.println("           "+gradeItemList[i]+averageList[i]);
 		}
 	}
-	
+	/**getStudentName usage
+	 * return the name of the student ID to search.
+	 * @param ID - ID to search.
+	 * @return student name.
+	 * 
+	 * pseudo code: 
+	 * 1.go through the List and find the matches ID.
+	 * 2.return the name.
+	 * 
+	 * Time estimate: O(N), N for number of students. 
+	 */
 	public String getStudentName(String ID) {
 		for (Grades temp : aList) {
             if(temp.ID.equals(ID)) return temp.name;
--- src/gradeSystem/Grades.java
+++ src/gradeSystem/Grades.java
@@ -5,7 +5,23 @@ public class Grades {
 	int[] gradeList;
 	int totalGrade;
 	
-
+	/**Constructor Usage
+	 * construct and initialize a structure to store students' grade.
+	 * 
+	 * @param ID - ID for the student.
+	 * @param name - name for the student.
+	 * @param lab1 - lab1 for the student.
+	 * @param lab2 - lab2 for the student.
+	 * @param lab3 - lab3 for the student.
+	 * @param midTerm - midTerm for the student.
+	 * @param finalExam - finalExam for the student.
+	 * 
+	 * pseudo code: 
+	 * 1.Initialize parameters, including ID, name, and five grades.
+	 * 2.The five grades are store in a list called gradeList.
+	 * 
+	 * Time estimate: O(1), constant time to initialize a student's information. 
+	 */
 	public Grades(String ID, String name, int lab1, int lab2, int lab3, int midTerm, int finalExam) {
 		this.ID = ID;
 		this.name = name;
@@ -17,6 +33,18 @@ public class Grades {
 		gradeList[4] = finalExam;
 	}
 	
+	/** calculateTotalGrade usage 
+	 * calculate the total grade of the student.
+	 * 
+	 * @param weights
+	 * @return totalGrade
+	 * 
+	 * pseudo code: 
+	 * 1.all five grades are multiplied with their weight.
+	 * 2.sum up all weighted grades.
+	 * 
+	 * Time estimate: O(1), constant time to calculate a total grade.
+	 */
 	public int calculateTotalGrade(float[] weights) {
 		float temp = 0;
 		for(int i=0;i<5;i++) temp += gradeList[i]*weights[i];
